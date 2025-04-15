c6ab27dfc94c28288d48d6cd9adc800d3ad7f044
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2025-04-14 19:30:49 -0500

    Disable Rapify

--- LaunchTestEnv.bat
+++ LaunchTestEnv.bat
@@ -3,6 +3,6 @@ REM Change to the directory where this batch file sits
 cd /d %~dp0
 
 echo Launching Arma 3 (ACE Arsenal Tutorial)...
-.\hemtt.exe launch arsenal
+.\hemtt.exe launch arsenal --no-rap
 
 pause
