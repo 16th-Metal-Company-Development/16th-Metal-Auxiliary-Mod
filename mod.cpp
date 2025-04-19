e43df1824132ff6dd018eacd307819030df718ad
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2025-04-19 17:09:16 -0500

    Include Subfiles

---
+++ mod.cpp
@@ -0,0 +1,16 @@
+name = "The 16th Metal Company Aux Mod";
+
+
+picture = "16th_logo.paa";
+
+
+logo = "16th_logo.paa"; 
+
+
+tooltip = "16th Metal Company"; 
+
+
+overview = "The 16th Metal Company Aux Mod"; 
+
+
+author = "Mod LCDR Wolf, Ronin, and Blatt";
