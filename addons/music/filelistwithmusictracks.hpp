91bf0d71906ac714fdf7337132b53d3f6a5c5b4d
LCDRWolf <halowarsgto2.0@gmail.com>
2025-11-14 16:17:29 -0500

    4.2.3

--- addons/music/filelistwithmusictracks.hpp
+++ addons/music/filelistwithmusictracks.hpp
@@ -1,259 +1,259 @@
 class Arena
 {
 	name = "06 - Arena";
-	sound[] = {"z\16th\addons\music\songs\arena.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\arena.ogg",db+0,1};
 	duration=93;
 	musicClass = "Tron";
 };
 class Armory
 {
 	name = "05 - Armory";
-	sound[] = {"z\16th\addons\music\songs\armory.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\armory.ogg",db+0,1};
 	duration=123;
 	musicClass = "Tron";
 };
 class bbd_fall
 {
 	name = "02 - Fall - Big Black Delta";
-	sound[] = {"z\16th\addons\music\songs\bbd.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\bbd.ogg",db+0,1};
 	duration=236;
 	musicClass = "Tron";
 };
 class clu
 {
 	name = "18 - CLU";
-	sound[] = {"z\16th\addons\music\songs\clue.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\clu.ogg",db+0,1};
 	duration=279;
 	musicClass = "Tron";
 };
 class derezzed
 {
 	name = "13 - Derezzed";
-	sound[] = {"z\16th\addons\music\songs\derezzed.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\derezzed.ogg",db+0,1};
 	duration=104;
 	musicClass = "Tron";
 };
 class derezzed_avicii
 {
 	name = "13 - Derezzed - Avicii";
-	sound[] = {"z\16th\addons\music\songs\derezzed_avicii.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\derezzed_avicii.ogg",db+0,1};
 	duration=306;
 	musicClass = "Tron";
 };
 class discwars
 {
 	name = "17 - Disc Wars";
-	sound[] = {"z\16th\addons\music\songs\discwars.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\discwars.ogg",db+0,1};
 	duration=252;
 	musicClass = "Tron";
 };
 class flynnlives
 {
 	name = "20 - Flynn Lives";
-	sound[] = {"z\16th\addons\music\songs\flynnlives.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\flynnlives.ogg",db+0,1};
 	duration=202;
 	musicClass = "Tron";
 };
 class kaskade_rinzler
 {
 	name = "09 - Rinzler - Kaskade";
-	sound[] = {"z\16th\addons\music\songs\kaskade_rinzler.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\kaskade_rinzler.ogg",db+0,1};
 	duration=414;
 	musicClass = "Tron";
 };
 class kitheory_sonofflynn
 {
 	name = "05 - Son of Flynn - Ki Theory";
-	sound[] = {"z\16th\addons\music\songs\kitheory_sonofflynn.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\kitheory_sonofflynn.ogg",db+0,1};
 	duration=293;
 	musicClass = "Tron";
 };
 class moby_sonofflynn
 {
 	name = "07 - Son of Flynn - Moby";
-	sound[] = {"z\16th\addons\music\songs\moby_sonofflynn.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\moby_sonofflynn.ogg",db+0,1};
 	duration=393;
 	musicClass = "Tron";
 };
 class outlands
 {
 	name = "09 - Outlands";
-	sound[] = {"z\16th\addons\music\songs\outlands.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\outlands.ogg",db+0,1};
 	duration=163;
 	musicClass = "Tron";
 };
 class photek_endofline
 {
 	name = "11 - End of Line - Photek";
-	sound[] = {"z\16th\addons\music\songs\photek_endofline.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\photek_endofline.ogg",db+0,1};
 	duration=320;
 	musicClass = "Tron";
 };
 class recognizer
 {
 	name = "04 - Recognizer";
-	sound[] = {"z\16th\addons\music\songs\recognizer.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\recognizer.ogg",db+0,1};
 	duration=158;
 	musicClass = "Tron";
 };
 class rinzler
 {
 	name = "07 - Riznler";
-	sound[] = {"z\16th\addons\music\songs\rinzler.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\rinzler.ogg",db+0,1};
 	duration=138;
 	musicClass = "Tron";
 };
 class sander_kleinenberg_end_titles_hd
 {
 	name = "15 - End Titles HD - Sander Kleinenberg";
-	sound[] = {"z\16th\addons\music\songs\sander_kleinenberg_end_titles_hd.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\sander_kleinenberg_end_titles_hd.ogg",db+0,1};
 	duration=306;
 	musicClass = "Tron";
 };
 class se_castor
 {
 	name = "Special Edition 04 - Castor";
-	sound[] = {"z\16th\addons\music\songs\se_castor.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\se_castor.ogg",db+0,1};
 	duration=140;
 	musicClass = "Tron";
 };
 class solarsailor
 {
 	name = "15 - Solar Sailer";
-	sound[] = {"z\16th\addons\music\songs\solarsailor.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\solarsailor.ogg",db+0,1};
 	duration=163;
 	musicClass = "Tron";
 };
 class thegamehaschanged
 {
 	name = "08 - The Game Has Changed";
-	sound[] = {"z\16th\addons\music\songs\thegamehaschanged.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\thegamehaschanged.ogg",db+0,1};
 	duration=206;
 	musicClass = "Tron";
 };
 class thesonofflynn
 {
 	name = "03 - The Son of Flynn";
-	sound[] = {"z\16th\addons\music\songs\thesonofflynn.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\tron\thesonofflynn.ogg",db+0,1};
 	duration=96;
 	musicClass = "Tron";
 };
 class tonsofawesome
 {
 	name = "2500 Tons of Awesome";
-	sound[] = {"z\16th\addons\music\songs\2500tonsofawesome.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\2500tonsofawesome.ogg",db+0,1};
 	duration=65;
 	musicClass = "PacificRim";
 };
 class callmenewt
 {
 	name = "Call me Newt";
-	sound[] = {"z\16th\addons\music\songs\callmenewt.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\callmenewt.ogg",db+0,1};
 	duration=102;
 	musicClass = "PacificRim";
 };
 class cancellingtheapocolypse
 {
 	name = "Cancelling the Apocalypse";
-	sound[] = {"z\16th\addons\music\songs\cancellingtheapocolypse.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\cancellingtheapocolypse.ogg",db+0,1};
 	duration=217;
 	musicClass = "PacificRim";
 };
 class categoryfive
 {
 	name = "Category Five";
-	sound[] = {"z\16th\addons\music\songs\categoryfive.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\categoryfive.ogg",db+0,1};
 	duration=136;
 	musicClass = "PacificRim";
 };
 class doubleevent
 {
 	name = "Double Event";
-	sound[] = {"z\16th\addons\music\songs\doubleevent.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\doubleevent.ogg",db+0,1};
 	duration=147;
 	musicClass = "PacificRim";
 };
 class gipsydanger
 {
 	name = "Gipsy Danger";
-	sound[] = {"z\16th\addons\music\songs\gipsydanger.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\gipsydanger.ogg",db+0,1};
 	duration=198;
 	musicClass = "PacificRim";
 };
 class gobigorgoextinct
 {
 	name = "Go Big or Go Extinct";
-	sound[] = {"z\16th\addons\music\songs\gobigorgoextinct.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\gobigorgoextinct.ogg",db+0,1};
 	duration=146;
 	musicClass = "PacificRim";
 };
 class justamemory
 {
 	name = "Just a Memory";
-	sound[] = {"z\16th\addons\music\songs\justamemory.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\justamemory.ogg",db+0,1};
 	duration=127;
 	musicClass = "PacificRim";
 };
 class nopulse
 {
 	name = "No Pulse";
-	sound[] = {"z\16th\addons\music\songs\nopulse.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\nopulse.ogg",db+0,1};
 	duration=58;
 	musicClass = "PacificRim";
 };
 class pacificrimtheme
 {
 	name = "Pacific Rim Theme";
-	sound[] = {"z\16th\addons\music\songs\pacificrimtheme.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\pacificrimtheme.ogg",db+0,1};
 	duration=295;
 	musicClass = "PacificRim";
 };
 class strikereureka
 {
 	name = "Striker Eureka";
-	sound[] = {"z\16th\addons\music\songs\strikereureka.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\strikereureka.ogg",db+0,1};
 	duration=115;
 	musicClass = "PacificRim";
 };
 class wearetheresistance
 {
 	name = "We are the Resistance";
-	sound[] = {"z\16th\addons\music\songs\wearetheresistance.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\wearetheresistance.ogg",db+0,1};
 	duration=110;
 	musicClass = "PacificRim";
 };
 class wecreatedmonsters
 {
 	name = "To Fight Monsters, We Created Monsters";
-	sound[] = {"z\16th\addons\music\songs\wecreatedmonsters.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\pacificrim\wecreatedmonsters.ogg",db+0,1};
 	duration=110;
 	musicClass = "PacificRim";
 };
 class cominhome
 {
 	name = "I'm Comin Home";
-	sound[] = {"z\16th\addons\music\songs\cominhome.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\other\cominhome.ogg",db+0,1};
 	duration=196;
 	musicClass = "other";
 };
 class downinthevalleytopray
 {
 	name = "Down in the Valley to Pray";
-	sound[] = {"z\16th\addons\music\songs\downinthevalleytopray.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\other\downinthevalleytopray.ogg",db+0,1};
 	duration=134;
 	musicClass = "other";
 };
 class hd_dangerzone
 {
 	name = "Hollow Drive Danger Zone";
-	sound[] = {"z\16th\addons\music\songs\hd_dangerzone.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\other\hd_dangerzone.ogg",db+0,1};
 	duration=195;
 	musicClass = "other";
 };
 class human
 {
 	name = "I'm only Human";
-	sound[] = {"z\16th\addons\music\songs\human.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\other\human.ogg",db+0,1};
 	duration=196;
 	musicClass = "other";
 };
@@ -267,269 +267,98 @@ class dancin
 class desperatemeasures
 {
     name = "We're the desperate measures (Metal)";
-    sound[] = {"z\16th\addons\music\songs\werethedesperatemeasures.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\werethedesperatemeasures.ogg",db+0,1};
     duration=233;
     musicClass = "Halo";
 };
 class iknowyou
 {
     name = "I know you";
-    sound[] = {"z\16th\addons\music\songs\iknowyou.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\iknowyou.ogg",db+0,1};
     duration=136;
     musicClass = "Halo";
 };
 class engaged
 {
     name = "Engaged";
-    sound[] = {"z\16th\addons\music\songs\engaged.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\engaged.ogg",db+0,1};
     duration=98;
     musicClass = "Halo";
 };
 class beholdapalehorse
 {
     name = "Behold, a pale horse";
-    sound[] = {"z\16th\addons\music\songs\beholdapalehorse.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\beholdapalehorse.ogg",db+0,1};
     duration=338;
     musicClass = "Halo";
 };
 class farthetsoutpost
 {
     name = "Farthest Outpost";
-    sound[] = {"z\16th\addons\music\songs\farthetsoutpost.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\farthetsoutpost.ogg",db+0,1};
     duration=314;
     musicClass = "Halo";
 };
 class lightofaidan
 {
     name = "Light of Aidan";
-    sound[] = {"z\16th\addons\music\songs\lightofaidan.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\lightofaidan.ogg",db+0,1};
     duration=150;
     musicClass = "Halo";
 };
 class onefinaleffort
 {
     name = "One Final Effort";
-    sound[] = {"z\16th\addons\music\songs\onefinaleffort.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\onefinaleffort.ogg",db+0,1};
     duration=188;
     musicClass = "Halo";
 };
 class devilsmonsters
 {
     name = "Devils... Monsters...";
-    sound[] = {"z\16th\addons\music\songs\devilsmonsters.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\devilsmonsters.ogg",db+0,1};
     duration=88;
     musicClass = "Halo";
 };
 class spv3devilsmonsters
 {
     name = "Devils... Monsters... (From SPV3)";
-    sound[] = {"z\16th\addons\music\songs\spv3devilsmonsters.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\spv3devilsmonsters.ogg",db+0,1};
     duration=96;
     musicClass = "Halo";
 };
 class tipofthespear
 {
     name = "Tip of the Spear";
-    sound[] = {"z\16th\addons\music\songs\tipofthespear.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\tipofthespear.ogg",db+0,1};
     duration=365;
     musicClass = "Halo";
 };
 class werethedesperatemeasuresog
 {
     name = "We're the Desperate Measures";
-    sound[] = {"z\16th\addons\music\songs\werethedesperatemeasureog.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\halo\werethedesperatemeasureog.ogg",db+0,1};
     duration=338;
     musicClass = "Halo";
 };
 class brotherhoodofsteel
 {
 	name = "Fallout TV Show: Brotherhood of Steel Suite";
-	sound[] = {"z\16th\addons\music\songs\brotherhoodofsteel.ogg",db+0,1};
+	sound[] = {"z\16th\addons\music\songs\fallout\brotherhoodofsteel.ogg",db+0,1};
     duration=385;
     musicClass = "Fallout";
 };
 class dontfearmw3
 {
     name = "Don't Fear the Reaper (MW3)";
-    sound[] = {"z\16th\addons\music\songs\dontfearmw3.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\other\dontfearmw3.ogg",db+0,1};
     duration=245;
     musicClass = "Other";
 };
 class monsterslowed
 {
     name = "Monster Slowed";
-    sound[] = {"z\16th\addons\music\songs\monster_slowed.ogg",db+0,1};
+    sound[] = {"z\16th\addons\music\songs\other\monster_slowed.ogg",db+0,1};
     duration=205;
     musicClass = "Other";
-};
-/*===================================================================================================================================================
-== The Diabolical Beats
-===================================================================================================================================================*/
-class stressedout
-{
-    name = "01 Stressed Out";
-    sound[] = {"z\16th\addons\music\songs\questionable\01_stressed_out.ogg",db+0,1};
-    duration=201;
-    musicClass = "Questionable";
-};
-class crankdat
-{
-    name = "02 Crank Dat";
-    sound[] = {"z\16th\addons\music\songs\questionable\02_crank_dat.ogg",db+0,1};
-    duration=221;
-    musicClass = "Questionable";
-};
-class kkkk
-{
-    name = "03 KKKK";
-    sound[] = {"z\16th\addons\music\songs\questionable\03_kkkk.ogg",db+0,1};
-    duration=223;
-    musicClass = "Questionable";
-};
-class bitchplease
-{
-    name = "04 Bitch Please";
-    sound[] = {"z\16th\addons\music\songs\questionable\04_bitch_please.ogg",db+0,1};
-    duration=233;
-    musicClass = "Questionable";
-};
-class allthatiwant
-{
-    name = "05 All That I want";
-    sound[] = {"z\16th\addons\music\songs\questionable\05_all_that_i_want.ogg",db+0,1};
-    duration=213;
-    musicClass = "Questionable";
-};
-class loseyourself
-{
-    name = "06 Lose Yourself";
-    sound[] = {"z\16th\addons\music\songs\questionable\06_lose_yourself.ogg",db+0,1};
-    duration=315;
-    musicClass = "Questionable";
-};
-class blacklivesdontmatter
-{
-    name = "07 Black Lives Don't Matter";
-    sound[] = {"z\16th\addons\music\songs\questionable\07_black_lives_dont_matter.ogg",db+0,1};
-    duration=201;
-    musicClass = "Questionable";
-};
-class partyinthekkk
-{
-    name = "08 Party In The KKK";
-    sound[] = {"z\16th\addons\music\songs\questionable\08_party_in_the_kkk.ogg",db+0,1};
-    duration=200;
-    musicClass = "Questionable";
-};
-class cottonplants
-{
-    name = "09 Cotton Plants";
-    sound[] = {"z\16th\addons\music\songs\questionable\09_cotton_plants.ogg",db+0,1};
-    duration=192;
-    musicClass = "Questionable";
-};
-class wearenumberone
-{
-    name = "10 We Are Number One";
-    sound[] = {"z\16th\addons\music\songs\questionable\10_we_are_number_one.ogg",db+0,1};
-    duration=170;
-    musicClass = "Questionable";
-};
-class moonmaniscomingtotown
-{
-    name = "11 Moon Man Is Coming to Town";
-    sound[] = {"z\16th\addons\music\songs\questionable\11_moon_man_is_coming_to_town.ogg",db+0,1};
-    duration=113;
-    musicClass = "Questionable";
-};
-class santamoon
-{
-    name = "12 Santa Moon";
-    sound[] = {"z\16th\addons\music\songs\questionable\12_santa_moon.ogg",db+0,1};
-    duration=67;
-    musicClass = "Questionable";
-};
-class jinglebells
-{
-    name = "13 Jingle Bells";
-    sound[] = {"z\16th\addons\music\songs\questionable\13_jingle_bells.ogg",db+0,1};
-    duration=130;
-    musicClass = "Questionable";
-};
-class illegalsinmyyard
-{
-    name = "14 Illegals in my Yard";
-    sound[] = {"z\16th\addons\music\songs\questionable\14_illegals_in_my_yard.ogg",db+0,1};
-    duration=165;
-    musicClass = "Questionable";
-};
-class trief
-{
-    name = "15 Trief";
-    sound[] = {"z\16th\addons\music\songs\questionable\15_trief.ogg",db+0,1};
-    duration=199;
-    musicClass = "Questionable";
-};
-class bitchplease2
-{
-    name = "16 Bitch Please 2";
-    sound[] = {"z\16th\addons\music\songs\questionable\16_bitch_please_2.ogg",db+0,1};
-    duration=286;
-    musicClass = "Questionable";
-};
-class tenfeethigher
-{
-    name = "17 Feet Higher";
-    sound[] = {"z\16th\addons\music\songs\questionable\17_ten_feet_higher.ogg",db+0,1};
-    duration=200;
-    musicClass = "Questionable";
-};
-class removekebab
-{
-    name = "18 Remove Kebab";
-    sound[] = {"z\16th\addons\music\songs\questionable\18_remove_kebab.ogg",db+0,1};
-    duration=234;
-    musicClass = "Questionable";
-};
-class changes
-{
-    name = "19 Changes";
-    sound[] = {"z\16th\addons\music\songs\questionable\19_changes.ogg",db+0,1};
-    duration=233;
-    musicClass = "Questionable";
-};
-class notoriouskkk
-{
-    name = "20 Notorious KKK";
-    sound[] = {"z\16th\addons\music\songs\questionable\20_notorious_kkk.ogg",db+0,1};
-    duration=194;
-    musicClass = "Questionable";
-};
-class letskilltheretards
-{
-    name = "21 Let's Kill The Retards";
-    sound[] = {"z\16th\addons\music\songs\questionable\21_lets_kill_the_retards.ogg",db+0,1};
-    duration=205;
-    musicClass = "Questionable";
-};
-class deathsquads
-{
-    name = "22 Deathsquads After Dark";
-    sound[] = {"z\16th\addons\music\songs\questionable\22_deathsquads.ogg",db+0,1};
-    duration=175;
-    musicClass = "Questionable";
-};
-class stillkkk
-{
-    name = "23 Still KKK";
-    sound[] = {"z\16th\addons\music\songs\questionable\23_still_kkk.ogg",db+0,1};
-    duration=230;
-    musicClass = "Questionable";
-};
-class alabama
-{
-    name = "Alabama ------";
-    sound[] = {"z\16th\addons\music\songs\questionable\alabama.ogg",db+0,1};
-    duration=137;
-    musicClass = "Questionable";
 };
